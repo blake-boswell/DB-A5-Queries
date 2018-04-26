de51ce759f912429eede2af5a9cf6e29c4217e2d
Blake Boswell <blake.w.boswell@gmail.com>
2018-04-25 23:04:09 -0500

    Final version

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # DB-A5-Queries
+Project output:
+!cat boswell.sql
+SELECT * FROM Trip;
+@boswell.sql
+100
+Fayetteville
+20-APR-18
+21-APR-18
+250
+@boswell.sql
+5
+Fayetteville
+21-APR-18
+20-APR-18
+250
+@boswell.sql
+5
+Fayetteville
+20-APR-18
+21-APR-18
+25000
+SELECT * FROM Trip;
+@boswell.sql
+5
+Fayetteville
+20-APR-18
+21-APR-18
+250
+SELECT * FROM Trip;
--- boswell.sql
+++ boswell.sql
@@ -1,11 +1,13 @@
 -- a PL/SQL procedure that performs the following tasks in order to insert a record into the Trip table.
 
 -- The procedure accepts values for Emp_ID, To_City, Dep_Date, Return_Date, and Est_Cost attributes.
-ACCEPT emp_id PROMPT "Enter the employee ID";
-ACCEPT to_city PROMPT "Enter the destination city";
-ACCEPT dep_date PROMPT "Enter the departure date";
-ACCEPT return_date PROMPT "Enter the return date";
-ACCEPT est_cost PROMPT "Enter the estimated cost fro the trip";
+SET serveroutput on
+
+ACCEPT emp_id NUMBER PROMPT "Enter the employee ID:   ";
+ACCEPT to_city CHAR PROMPT "Enter the destination city:   ";
+ACCEPT dep_date DATE PROMPT "Enter the departure date (DD-MMM-YY format):   ";
+ACCEPT return_date DATE PROMPT "Enter the return date (DD-MMM-YY format):   ";
+ACCEPT est_cost BINARY_DOUBLE PROMPT "Enter the estimated cost for the trip:   ";
 
 DECLARE
     var_emp_id Trip.Emp_ID%type;
@@ -24,23 +26,55 @@ DECLARE
 BEGIN
     SELECT count(ID) INTO num_records FROM Employee WHERE ID = &emp_id;
     IF num_records = 0 THEN
-        dbms_output.put_line('[BAD job] That employee ID does not exist');
+        dbms_output.put_line('ERROR: That employee ID does not exist');
+        dbms_output.put_line('Aborting procedure...');
     ELSE 
-        dbms_output.put_line('[Good job] Success! That Employee ID already existed, good job master Boswell!');
-        IF TO_DATE(&return_date, 'Dd Mon YYYY') < TO_DATE(&dep_date, 'Dd Mon YYYY') THEN
-            dbms_output.put_line('[BAD job] Return date cannot be before the departure date! Our time travelling machine is broken!');
+        IF '&return_date' < '&dep_date' THEN
+            dbms_output.put_line('ERROR: Return date cannot be before the departure date!');
+            dbms_output.put_line('Aborting procedure...');
+            RETURN;
         ELSE
-            dbms_output.put_line('[Good job] Return date is after the departure date!');
             IF &est_cost >= 1 AND &est_cost <= 4000 THEN
-                dbms_output.put_line('[Good job] Estimated cost is within the range!');
                 SELECT MAX(ID) INTO old_id FROM Trip;
                 new_id := old_id + 1;
-                INSERT INTO Trip VALUES (new_id, &emp_id, &to_city, &dep_date, &return_date, &est_cost);
+                INSERT INTO Trip VALUES (new_id, &emp_id, '&to_city', '&dep_date', '&return_date', &est_cost);
                 dbms_output.put_line('Successfully inserted record!');
             ELSE
-                dbms_output.put_line('[BAD job] Estimated cost is NOT within the range!');
+                dbms_output.put_line('ERROR: Estimated cost must be less than 4,000.00 and greater than 1.00');
+                dbms_output.put_line('Aborting procedure...');
+                RETURN;
             END IF;
         END IF;
     END IF;
 END;
 /
+
+
+!cat boswell.sql
+SELECT * FROM Trip WHERE ID > 25;
+@boswell.sql
+100
+Fayetteville
+20-APR-18
+21-APR-18
+250
+@boswell.sql
+5
+Fayetteville
+21-APR-18
+20-APR-18
+250
+@boswell.sql
+5
+Fayetteville
+20-APR-18
+21-APR-18
+25000
+SELECT * FROM Trip WHERE ID > 25;
+@boswell.sql
+5
+Fayetteville
+20-APR-18
+21-APR-18
+250
+SELECT * FROM Trip WHERE ID > 25;
